dd73e709d815ffee3401aae23984924db2ad452c
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2024-02-11 06:03:48 +0700

    Add: Base Apparel Coming Soon Page solution

---
+++ pages/base-apparel-coming-soon-page/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.querySelector('input[type="email"]')
+  const btn = document.querySelector('button')
+  const textError = document.querySelector('p.text-error')
+
+  input.addEventListener('focus', () => {
+    input.parentElement.classList.remove('error')
+  })
+  btn.addEventListener('click', () => {
+    const emailRegex =
+      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+    if (input.value.trim() === 0 || !emailRegex.test(input.value)) {
+      input.parentElement.classList.add('error')
+      textError.innerHTML = 'Please provide a valid email address'
+    }
+  })
+})
