274da51371a46f22eba25488dc6e941e9a279658
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2024-02-19 04:07:30 +0700

    Add: PING Coming Soon Page solution

---
+++ pages/ping-coming-soon-page/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+  const form = document.querySelector('form')
+  const emailInput = document.querySelector('input.email-input')
+
+  emailInput.addEventListener('input', (event) => {
+    emailInput.classList.remove('is-error')
+  })
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    emailInput.classList.remove('is-error')
+    if (!emailRegex.test(emailInput.value)) {
+      emailInput.classList.add('is-error')
+    }
+  })
+})
